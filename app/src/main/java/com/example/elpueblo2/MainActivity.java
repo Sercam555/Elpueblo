53d2c5371ee13e3aaf019b1561d8c11d7b2d55f5
epiler <epiler447@gmail.com>
2023-05-19 20:35:43 -0500

    se crean splash

--- app/src/main/java/com/example/elpueblo2/MainActivity.java
+++ app/src/main/java/com/example/elpueblo2/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.elpueblo2;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity {
+    //Aquí vamos a poner LOS ATRIBUTOS = VARIABLES
+    MediaPlayer cancion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cancion=MediaPlayer.create(this,R.raw.audio1);
+        cancion.start();
+
+
+        //Temporizar el lanzamiento de la canción
+        TimerTask inicioAPP= new TimerTask() {
+            @Override
+            //esto es un metodo
+            public void run() {
+
+                Intent intent=new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(inicioAPP,15000);
     }
 }
